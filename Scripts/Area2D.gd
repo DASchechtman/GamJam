530daa9a4d74f74a322914a5c982087bf6b1bd57
Daniel Schechtman <dschechtman@carthage.edu>
2024-03-10 01:03:14 -0600

    finally got character to collide with ground

--- Scripts/Area2D.gd
+++ Scripts/Area2D.gd
@@ -1,14 +1,15 @@
-extends CharacterBody2D
+extends Area2D
 
-var x: int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_x):
+func _process(_delta):
 	pass
 
-func _physics_process(_delta):
-	move_and_collide(Vector2(0, 1))
+
+func _on_body_entered(_body:Node2D):
+	print("body entered")
